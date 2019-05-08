0d5b7e6bb9162a7692c530d7b6ba034c8e1c4b54
cwflove <1358253177@qq.com>
2019-05-08 15:27:21 +0800

    添加注释

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 
+//https://github.com/cwflove/flutter.git
+//主函数（函数入口）
 void main()=> runApp(MyApp());
-
+//声明MyApp类
 class MyApp extends StatelessWidget{
+  //重写build方法
   @override
   Widget build(BuildContext context){
-
+    //返回一个Material风格组件
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
+        //创建一个Bar,并添加文本
         appBar: AppBar(
           title: Text( 'Welcome to Flutter')
          ),
+         //在主题的中间区域，添加一个hello world
          body:Center(
            child: Text("Hello World"),
          ),
